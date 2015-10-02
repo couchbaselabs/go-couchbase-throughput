d22fce8e7530111d1b3612965f63a9cb5e352c7e
Traun Leyden <tleyden@couchbase.com>
2015-10-02 14:27:20 -0700

    use 2 storage engines for go-cb

--- main.go
+++ main.go
@@ -6,11 +6,16 @@ var SlowServerCallWarningThreshold = time.Duration(200) * time.Millisecond
 
 func main() {
 
-	totalNumDocs := 1000000
+	var storageEngineWriter StorageEngine
+	var storageEngineReader StorageEngine
+
+	totalNumDocs := 500000
 	chanBufferSize := totalNumDocs
 	docSizeBytes := 100
 	numDocWriters := 100
 	numDocReaders := 100
+	storageEngineType := "gocb"
+	// storageEngineType := "go-couchbase"
 
 	// create docsToWrite and docsToRead and docsFinished channels
 	docsToWrite := make(chan Document, chanBufferSize)
@@ -25,20 +30,31 @@ func main() {
 	// create the storage engine (either go-couchbase or go-cb)
 	url := "http://127.0.0.1:8091"
 	bucket := "default"
-	storageEngine := NewGoCouchbaseStorageEngine(url, bucket)
-	// storageEngine := NewGoCBStorageEngine(url, bucket)
-	// storageEngine := NewMockStorageEngine(url, bucket)
+
+	switch storageEngineType {
+	case "go-couchbase":
+		storageEngineWriter = NewGoCouchbaseStorageEngine(url, bucket)
+		storageEngineReader = storageEngineWriter
+	case "gocb":
+		storageEngineWriter = NewGoCBStorageEngine(url, bucket)
+		storageEngineReader = NewGoCBStorageEngine(url, bucket)
+	case "mock":
+		storageEngineWriter = NewMockStorageEngine(url, bucket)
+		storageEngineReader = NewMockStorageEngine(url, bucket)
+	default:
+		panic("Unknown storage engine value")
+	}
 
 	// create a bunch of docWriter goroutines and pass the docsToWrite channel
 	// and the storage engine
-	createDocWriters(docsToWrite, docsToRead, storageEngine, numDocWriters)
+	createDocWriters(docsToWrite, docsToRead, storageEngineReader, numDocWriters)
 
 	// wait until all docs have been written
 	blockUntilAllDocsWritten(totalNumDocs, docsToRead)
 
 	// create a bunch of docReader goroutines and pass the docsToRead channel
 	// and the storage engine
-	createDocReaders(docsToRead, docsFinished, storageEngine, numDocReaders)
+	createDocReaders(docsToRead, docsFinished, storageEngineWriter, numDocReaders)
 
 	// wait for doc feeder goroutine to finish (wait group)
 	wg.Wait()
